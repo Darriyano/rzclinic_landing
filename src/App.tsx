743728d323aca50e5b4eeb0c1b9112834d7a5a36
darriyano <android.stepanova@gmail.com>
2025-02-22 23:40:32 +0300

    Predeployment

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import {Routing} from "./pages/Routes";
+
+function App() {
+    return (<>
+            <Routing/>
+        </>
+    );
+}
+
+export default App;
